30a3dec3f42088db3ea56a3268b356b210ca57c0
mazkmg <mazkmg22@gmail.com>
2020-07-02 15:53:24 +0300

    доработка сайта

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,110 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+
+function nextSlide() {
+  console.log(slideIndex);
+
+  if (slideIndex > slides.length - 1) {
+    slideIndex = 0;
+  } else {
+    if (slideIndex>=4)
+    {
+      slideIndex = 0
+    }
+    else
+    {
+      slideIndex++;
+    }
+      }
+
+  showSlides(slideIndex);
 }
+
+function prevSlide() {
+  console.log(slideIndex);
+
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  } else {
+    if (slideIndex<=0)
+    {
+      slideIndex = 4
+    }
+    else
+    {
+      slideIndex--;
+    }
+  }
+
+  showSlides(slideIndex);
+}
+
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(index) {
+  slideIndex = index;
+  showSlides(slideIndex);
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) 
-    {
-      slideIndex = 1
-    }
-  if (n < 1) 
-    {
-      slideIndex = slides.length
-    }
-  for (i = 0; i < slides.length; i++) 
-    {
-      slides[i].style.display = "none";
-    }
-  for (i = 0; i < dots.length; i++) 
-    {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += "active";
-}
+function showSlides(itemIndex) {
+  for (const slide of slides) {
+    slide.style.display = "none";
+  }
 
-function alert()
-{
-  console.log("работает")
+  for (const dot of dots) {
+    dot.classList.remove('active');
+  }
+
+  slides[itemIndex].style.display = "block";
+  dots[itemIndex].classList.add('active');
 }
 
 $(function(){
   showSlides(slideIndex);
-  alert()
 });
+
+
+function creatPopup(){
+  console.log ("Запускается всплывающее окно")
+  let modalCounter = 0;
+  let container = document.createElement("div");
+  let id = "modal_" + modalCounter;
+  let duration = $("#duration option:selected").text();
+  let place = $("#place option:selected").text();
+  let quantity = $("#quantity option:selected").text();
+  console.log (duration);
+  console.log (place);
+  console.log (quantity);
+
+  container.classList.add("modal");
+  container.innerHTML = `
+  <div class = "modal_content">
+  <div class = "modal_text_position">
+  <div class = "modal_text">
+  Вы выбрали: <br>
+  Дата поездки:
+  Место поездки: ${place} <br>
+  Срок поездки:  ${duration} <br>
+  Колличество участников: ${quantity} <br>
+  Стоимость тура: 
+  </div>
+  <div class = "modal_text">Укажите свой телефон, чтобы мы могли связаться с вами и оформить тур <br>
+  <div class="modal_input_position"><input type="text" name="email_input" class="modal_input" placeholder="Укажите номер телефона">
+  <button class="modal_button">Отправить</button></div></div>
+  </div>
+  <div class="close"><span>Х</span></div>
+  `;
+  $("#creatPopup").html(container);
+  console.log (container);
+  modalCounter++;
+  
+  
+  let closeButton = $("#" + id).children().children("span");
+
+  $(closeButton).on("click", function(){
+    document.getElementsById(id).remove();
+  });
+ 
+}
